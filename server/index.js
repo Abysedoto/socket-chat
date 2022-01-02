0881e9527f5643467edc9c4eace160df91bddfc0
Abysedoto <dimka111111111111111@gmail.com>
2022-01-02 17:39:03 +0400

    com3

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express');
 const app = express();
+const path = require('path')
 const server = require('http').Server(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -10,7 +11,8 @@ const io = require("socket.io")(server, {
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
-// app.use(express.static(__dirname, '/../build'))
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
 app.use(express.urlencoded({ extended: false }));
 
 let rooms = new Map()
